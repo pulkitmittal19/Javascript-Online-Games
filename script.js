65b63a0706ed8cbadd8bd444ea79cf4499a975e2
pulkitmittal19 <pulkitmittal19@gmail.com>
2020-05-09 17:25:28 +0530

    Add: ball move and break bricks functionality

--- script.js
+++ script.js
@@ -135,6 +135,50 @@ function movePaddle(){
     }
 }
 
+function moveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    // Wall collison for ball
+    if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0){
+        ball.dx *= -1; 
+    }
+
+    if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0){
+        ball.dy *= -1; 
+    }
+
+
+    // Paddle collision
+    if(
+        ball.x - ball.size > paddle.x &&
+        ball.x  + ball.size < paddle.x + paddle.w &&
+        ball.y + ball.size > paddle.y)
+        {
+            ball.dy = -ball.speed;
+        }
+
+    // Bricks collison
+    bricks.forEach(column => {
+        column.forEach(brick => {
+          if (brick.visible) {
+            if (
+              ball.x - ball.size > brick.x && // left brick side check
+              ball.x + ball.size < brick.x + brick.w && // right brick side check
+              ball.y + ball.size > brick.y && // top brick side check
+              ball.y - ball.size < brick.y + brick.h // bottom brick side check
+            ) {
+              ball.dy *= -1;
+              
+              brick.visible = false;
+    
+              
+            }
+          }
+        });
+      });
+}
+
 
 function draw(){
 
@@ -156,6 +200,7 @@ function draw(){
 function update(){
 
     movePaddle();
+    moveBall();
     // draw everything
     draw();
     requestAnimationFrame(update);
